9b40c754f856b1aea8edb69a58e98faf83abc603
samoei <samoeiayuka@gmail.com>
2022-06-09 19:39:20 +0200

    added navbar

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import './pages/landingpage.dart';
+
+void main() {
+  // ignore: unnecessary_new
+  runApp(new MaterialApp(
+    home: new LandingPage(),
+  ));
+}
