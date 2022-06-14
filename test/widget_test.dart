d70f82f96d3288746b08b987fd96d938151b5ff3
samoei <samoeiayuka@gmail.com>
2022-06-14 02:33:19 +0200

    commented out unnecessary test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,7 +14,7 @@ import 'package:see_me/pages/landingpage.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(myApp());
+    //await tester.pumpWidget(myApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
