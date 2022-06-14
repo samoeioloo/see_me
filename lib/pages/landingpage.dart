07bcb9213e0fafd1ff4e581110d6902224734232
samoei <samoeiayuka@gmail.com>
2022-06-14 02:24:31 +0200

    added button to landing pg to redirect to home pg

--- lib/pages/landingpage.dart
+++ lib/pages/landingpage.dart
@@ -1,10 +1,12 @@
 // home page
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_fadein/flutter_fadein.dart';
 import 'package:flutter_gradients/flutter_gradients.dart';
+
+import 'home.dart';
 
 class LandingPage extends StatelessWidget {
   double _margin = 0;
@@ -25,11 +27,12 @@ class LandingPage extends StatelessWidget {
       // Material widget = piece of paper
       appBar: AppBar(),
       body: AnimatedOpacity(
+        // ignore: sort_child_properties_last
         child: FadeIn(
           child: Container(
             width: 600.0,
             height: 600.0,
-            decoration: new BoxDecoration(
+            decoration: BoxDecoration(
                 gradient: FlutterGradients.paloAlto(), shape: BoxShape.circle),
             child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
@@ -46,7 +49,32 @@ class LandingPage extends StatelessWidget {
                     ),
                     duration: Duration(seconds: 5),
                     curve: Curves.easeIn,
-                  )
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.all(35),
+                      child: FadeIn(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              onPrimary: Colors.black,
+                              primary: Colors.green,
+                              shadowColor: Colors.green,
+                              elevation: 30,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30))),
+                          child: Text('Start here',
+                              style: GoogleFonts.roboto(color: Colors.white)),
+                          onPressed: () {
+                            // switch to new route
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => HomePage()),
+                            );
+                          },
+                        ),
+                        duration: Duration(seconds: 10),
+                        curve: Curves.easeIn,
+                      ))
                 ]),
           ),
           duration: Duration(seconds: 3),
@@ -55,6 +83,29 @@ class LandingPage extends StatelessWidget {
         //alignment: Alignment.center,
         duration: Duration(seconds: 3),
         opacity: _opacity,
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // close 2nd route and return to 1st
+            // pop() removes current Route from stack of routes managed by the Navigator
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
       ),
     );
   }
